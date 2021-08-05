ad5678ddf74a8b7b5b0fdedf05bffa68f522cede
Kanymgul <jamalova8891@gmail.com>
2021-08-05 16:37:50 +0600

    qwerty

--- src/components/card_sneakers/cardSneakers.jsx
+++ src/components/card_sneakers/cardSneakers.jsx
@@ -1,28 +1,44 @@
 import React, { useState} from "react"
 import css from  "./cardSneakers.module.css"
 
-export const CardSneakers = () => {
+export const CardSneakers = (props) => {
     const [show, setShow] = useState(false)
     const [check, setCheck] = useState(false)
 
+    const addBasket = () => {
+        props.addBasket({title:props.title, imageUrl:props.imageUrl, price:props.price})
+    }
+    const addFavour = () => {
+        props.addFavour({title:props.title, imageUrl:props.imageUrl, price:props.price})
+    } 
+
     return (
         <div>
+            <div className={css.cards}>
             <div className={css.card_container}>
                 <div className={css.img}>
-                    <img src="../../images/green-boots.svg" alt="" />
+                    <img src={props.imageUrl} alt="" />
                 </div>
                 <div className={css.love_img}>
-                    {!check && <img src="../../images/like.svg" alt="" onClick={() => setCheck(!check)} />}
+                    {!check && <img src="../../images/like.svg" alt="" onClick={() => {
+                        setCheck(!check);
+                        addFavour();
+
+                    } }/>}
                     {check && <img src="../../images/pink_love.svg" alt="" onClick={() => setCheck(!check)} />}
 
                 </div>
-                <p className={css.card_desc}>Мужские Кроссовки Nike Blazer Mid Suede</p>
+                <p className={css.card_desc}>{props.title}</p>
                 <p className={css.price}>Цена:</p>
-                <p className={css.card_price}>12 999 руб.</p>
+                <p className={css.card_price}>{props.price} руб.</p>
                 <div className={css.plus_img}>
-                {!show && <img src="../../images/+.svg" alt="" onClick={() => setShow(!show)} />}
+                {!show && <img src="../../images/+.svg" alt="" onClick={() => {
+                    setShow(!show);
+                    addBasket();
+                    } } />}
                     {show && <img src="../../images/check.svg" alt="" onClick={() => setShow(!show)} />}
                 </div>
+            </div>
             </div>
         </div>
     )}
--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState, useEffect} from 'react'
 import { CardSneakers } from '../../components/card_sneakers/cardSneakers'
 import Slider from '../slider/slider'
 import css from "./main.module.css"
@@ -6,6 +7,31 @@ import css from "./main.module.css"
 
 
 export const Main = () => {
+
+    const [items, setItems] = useState([])
+    const [cart, setCart] = useState([])
+    const [favour, setFavour] = useState([])
+
+    useEffect(() => {
+      axios.get("https://60f11cfd38ecdf0017b0fa2e.mockapi.io/cards").then((response) => {
+          setItems(response.data)
+      })
+    }, [])
+
+    function addBasket(obj) {
+        axios.post("https://60f11cfd38ecdf0017b0fa2e.mockapi.io/cart", obj)
+        .then((res) => console.log(res))
+        setCart((prev => ([...prev, obj])))
+    }
+
+    function addFavour(obj) {
+        axios.post("https://60f11cfd38ecdf0017b0fa2e.mockapi.io/cars", obj)
+        .then((res) => console.log(res))
+        setFavour((prev => ([...prev, obj])))
+
+    }
+
+
     return (
         <div className={css.container}>
             <Slider/>
@@ -20,7 +46,16 @@ export const Main = () => {
                 </div>
                
             </div>
-            <CardSneakers/>
+            <div>
+                {items.map((item) => (
+                    <CardSneakers addBasket={addBasket} addFavour={addFavour}
+                       key={item.id}
+                       title={item.title}
+                       imageUrl={item.imageUrl}
+                       price={item.price}
+                    />
+                    ))}
+            </div>
         </div>
     )
 }
